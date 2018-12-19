f33f1a33f8cd6eee57164546b8d4622d3168982d
Jackson de Fraga <jackson.fraga86@gmail.com>
2018-12-19 16:18:12 -0200

    add whats feature

---
+++ src/components/Whatsapp.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="whatsapp">
+    <h2>{{ msg }}</h2>
+    <input v-model="fone" placeholder="+5551999998888">
+    <a v-bind:href="url">Send Message</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WhatsApp',
+  data () {
+    return {
+      msg: 'Abrir conversa whatsapp sem contato',
+      fone: ''
+
+    }
+  },
+  computed: {
+    url () {
+      return 'whatsapp://send?abid=' + this.fone + '&text=Olá!'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
